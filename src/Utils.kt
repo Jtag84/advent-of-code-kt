ab3104f0f95d6676f5ba5c817000d68840e95f23
Clement <vasseur.clement@gmail.com>
2023-12-01 18:58:29 -0500

    Day 1 2023

--- src/Utils.kt
+++ src/Utils.kt
@@ -1,12 +1,16 @@
 import java.math.BigInteger
 import java.security.MessageDigest
+import java.time.LocalDate
 import kotlin.io.path.Path
 import kotlin.io.path.readLines
 
 /**
  * Reads lines from the given input txt file.
  */
-fun readInput(name: String) = Path("src/$name.txt").readLines()
+fun readInput(day: LocalDate) = Path("${getPathPrefixString(day)}.txt").readLines()
+fun readTestInput(day: LocalDate) = Path("${getPathPrefixString(day)}_test.txt").readLines()
+
+private fun getPathPrefixString(day: LocalDate) = "src/year${day.year}/Day${String.format("%02d", day.dayOfMonth)}"
 
 /**
  * Converts string to md5 hash.
