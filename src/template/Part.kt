425c2f0b2d28aa286c1833449bee05a42adf73bd
Clement <vasseur.clement@gmail.com>
2023-12-09 10:23:23 -0500

    updated template

--- src/template/Part.kt
+++ src/template/Part.kt
@@ -3,11 +3,11 @@ package <PACKAGE_NAME>
 import Part.Companion.part<PART_NUMBER>
 
 fun main() {
-    part<PART_NUMBER>.runTest(1)
+    part<PART_NUMBER>.runTest()
     part<PART_NUMBER>.run()
 }
 
-val part<PART_NUMBER> = part<PART_NUMBER>(inputParser) { input ->
+val part<PART_NUMBER> = part<PART_NUMBER>(inputParser, 1) { input ->
     input
 }
 
