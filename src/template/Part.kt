5d6f155d64c6f007ea559ebdffe6fff1030e6ae9
Clement <vasseur.clement@gmail.com>
2023-12-19 17:51:01 -0500

    template update

--- src/template/Part.kt
+++ src/template/Part.kt
@@ -3,8 +3,8 @@ package <PACKAGE_NAME>
 import commons.Part.Companion.part<PART_NUMBER>
 
 fun main() {
-    part<PART_NUMBER>.runTest()
-    part<PART_NUMBER>.run()
+    part<PART_NUMBER>.runAndPrintTest()
+    part<PART_NUMBER>.runAndPrint()
 }
 
 val part<PART_NUMBER> = part<PART_NUMBER>(inputParser, 1) { input ->
